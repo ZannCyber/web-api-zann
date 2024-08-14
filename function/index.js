dfbadc2fca3369870be79807243ae53ade3253e0
ZannMods <162866065+ZannCyber@users.noreply.github.com>
2024-08-15 02:52:38 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -1,9 +1,8 @@
 module.exports = {
-	blackboxAIChat: require('./scraper/blackboxAIChat'),
-	smartContract: require('./scraper/smartContract'),
-	ragBot: require('./scraper/ragBot'),
-	tiktokDownloader: require('./scraper/tiktokDownloader')
-	
-
-
-}
+    blackboxAIChat: require('./scraper/blackboxAIChat'),
+    smartContract: require('./scraper/smartContract'),
+    ragBot: require('./scraper/ragBot'),
+    tiktokDownloader: require('./scraper/tiktokDownloader'),
+    Gempa: require('./scraper/Gempa'),  // Tambahkan fungsi baru
+    anotherFunction: require('./scraper/anotherFunction'),  // Tambahkan fungsi baru lainnya
+};
