8a281596ae107ba02f5f0e180265e2575e649399
ZannMods <162866065+ZannCyber@users.noreply.github.com>
2024-08-16 00:43:54 +0700

    Update index.js

--- function/index.js
+++ function/index.js
@@ -6,6 +6,7 @@ module.exports = {
     Gempa: require('./scraper/Gempa'),  
     ytdl: require('./scraper/ytdl'),
     tiktokScraper: require('./scraper/tiktokScraper'),
+    youtubev3: require('./scraper/youtubev3'),
     // Tambahkan fungsi baru
     
 };
